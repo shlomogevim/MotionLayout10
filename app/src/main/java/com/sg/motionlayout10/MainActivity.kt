42050c1367109da7deead5223099cbd1ad3bfcb2
shlomo <shlomo.gevim@gmail.com>
2021-05-21 21:42:03 +0300

    Initial commit

---
+++ app/src/main/java/com/sg/motionlayout10/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.sg.motionlayout10
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        description_txt.setText(SUPRE_STRING)
+
+    }
+}
